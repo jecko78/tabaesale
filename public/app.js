692671f589b797642f8a55e98ccde01219de1f1a
jecko78 <jecko78@gmail.com>
2025-10-24 21:32:07 +0700

    Authentication completed

--- public/app.js
+++ public/app.js
@@ -1,6 +1,6 @@
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
-  import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
+  import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged, signOut, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-analytics.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,6 +21,11 @@
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
   const auth = getAuth(app);
+  const profile = document.getElementById('profile');
+  const useremail = document.getElementById('useremail');
+  const logoutbtn = document.getElementById('logoutbtn');
+  const loginForm = document.getElementById('loginForm');
+
 
   const form = document.getElementById('registerform');
   const formarea = document.getElementById('formarea');
@@ -39,3 +44,40 @@
         // console.log(form.email.value);
         // console.log(form.password.value);
     });
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // User is signed in
+      const uid = user.uid;
+      profile.style.display = "block";
+      formarea.style.display = "none";
+      useremail.innerText = `Welcome, ${user.email}`;
+      // ...
+    } else {
+      // User is signed out
+      profile.style.display = "none";
+      formarea.style.display = "block";
+      // ...
+    }
+  });
+  logoutbtn.addEventListener('click', (e) => {
+    // console.log("Logout clicked");
+    signOut(auth).then(() => {
+        // Sign-out successful.
+        alert("User Logged Out Successfully");
+      }).catch((error) => {
+        // An error happened.
+        alert(error.message);
+      });
+  });
+  loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = loginForm.email.value;
+    const password = loginForm.password.value;
+    // console.log(email, password);
+    signInWithEmailAndPassword(auth, email, password)
+    .then((result) => {
+        alert("User Logged In Successfully");
+    }).catch((error) => {
+        alert(error.message);
+    });
+  });
